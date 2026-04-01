921e0cd7e8985eab0545ed11baa7a99e5e0ef383
Doomsdreal <fauzan.20.annida@gmail.com>
2026-04-01 08:42:46 +0700

    failed popup translation

--- resources/views/views_dashboard_me.blade.php
+++ resources/views/views_dashboard_me.blade.php
@@ -199,7 +199,7 @@
     <!-- Page Info -->
     <script>
         document.addEventListener("DOMContentLoaded", () => {
-        showPageInfo("popup.dashboard");
-    });
+            showPageInfo("Kelola dashboard Anda di sini. Pantau aktivitas dan proyek terbaru.");
+        });
     </script>
 @endsection
