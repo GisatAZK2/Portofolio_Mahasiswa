90b1be59f0e0859cc51af7467252386bf49cd2a6
DimNih <dimaserlansyah5@gmail.com>
2026-04-22 13:14:15 +0700

    fix remake button

--- resources/views/views_dashboard.blade.php
+++ resources/views/views_dashboard.blade.php
@@ -57,7 +57,7 @@
                                             <path d="M12 2L15 8.5L22 9.5L17 14L18.5 21L12 17.5L5.5 21L7 14L2 9.5L9 8.5L12 2Z" stroke="currentColor" stroke-width="1.5" fill="none"/>
                                             <path d="M12 6L13.5 9.5L17.5 10L14.5 12.5L15.5 16.5L12 14.5L8.5 16.5L9.5 12.5L6.5 10L10.5 9.5L12 6Z" fill="currentColor"/>
                                         </svg>
-                                        <span>{{ autoTranslate('Leaderboard Game') }}</span>
+                                        <span>{{ autoTranslate('Peringkat Permainan') }}</span>
                                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
                                         </svg>
