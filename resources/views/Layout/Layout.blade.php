247b58c7569aceb0f40258121719bc308053d7e6
GisatAZK2 <gisatazk2@gmail.com>
2026-04-27 15:33:18 +0700

    Fix viewport

--- resources/views/Layout/Layout.blade.php
+++ resources/views/Layout/Layout.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=0.9, user-scalable=no">
     <meta name="theme-color" content="#ffffff">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/Logo.svg') }}">
