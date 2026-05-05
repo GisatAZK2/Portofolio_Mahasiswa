492d69cdbc59e8a32a5bb2fd7d3a649bde370a4f
GisatAZK2 <gisatazk2@gmail.com>
2026-05-05 08:09:44 +0700

    Merge Branch

--- resources/views/views_portofolio_user.blade.php
+++ resources/views/views_portofolio_user.blade.php
@@ -190,6 +190,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2" />
                                 </svg>
                                 {{autotranslate('Postingannya')}}
+                                
                             </h3>
                             <span class="text-sm text-purple-600 dark:text-purple-400 font-medium">
                                 {{ $postingans->total() ?? 0 }} postingan
