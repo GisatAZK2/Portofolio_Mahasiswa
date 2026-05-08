806a50ca431fa5a2430685f9291d313868863c34
GisatAZK2 <gisatazk2@gmail.com>
2026-05-08 09:18:33 +0700

    Fix UI

--- resources/views/views_dashboard_me.blade.php
+++ resources/views/views_dashboard_me.blade.php
@@ -1311,8 +1311,8 @@ window.loadComments = async function(postinganId) {
 
             if (commentsArray.length === 0) {
                 const noCommentsText = (window.locale === 'id')
-                    ? '✨ Belum ada komentar. Jadilah yang pertama!'
-                    : '✨ No comments yet. Be the first!';
+                    ? 'Belum ada komentar. Jadilah yang pertama!'
+                    : 'No comments yet. Be the first!';
                 container.innerHTML = `<p class="text-sm text-gray-500 dark:text-gray-400 text-center py-5">${noCommentsText}</p>`;
                 return;
             }
