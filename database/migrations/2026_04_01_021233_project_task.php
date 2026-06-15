6bcc650121bb8f58d52c3271799af61c26a0dafd
GisatAZK2 <gisatazk2@gmail.com>
2026-06-15 15:34:08 +0700

    Change Type Data name_taskt From string To text

--- database/migrations/2026_04_01_021233_project_task.php
+++ database/migrations/2026_04_01_021233_project_task.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('project_id')
                   ->constrained('projects')
                   ->cascadeOnDelete();
-            $table->string('name_task');
+            $table->text('name_task');
             
             $table->boolean('is_done')->default(false);
             $table->foreignId('user_id')
